a175be1a5cd3e036c47e355f29f7c43002ddcc1f
Andrew Krueger <krueger.andrew@gmail.com>
2016-07-31 16:44:57 -0500

    Update webpack config and get index.html to render with dev-server

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	devtool: 'eval-source-map',
+	entry: './src/main.jsx',
+	output: {
+		path: './build',
+		filename: 'bundle.js'
+	},
+	module: {
+		preloaders: [{
+			test: /\.jsx?$/,
+			exclude: /node_modules/,
+			loader: 'eslint'
+		}],
+		loaders: [{
+			test: /\.jsx?$/,
+			exclude: /node_modules/,
+			loader: 'babel'
+		}]
+	}
+}
