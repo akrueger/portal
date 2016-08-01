8b9f01130886528b507295a381fc02118d523dbb
Andrew Krueger <krueger.andrew@gmail.com>
2016-08-01 02:54:01 -0500

    Add CSS packages and adjust Webpack config for loaders

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,14 +1,21 @@
+const webpack = require('webpack')
 const precss = require('precss')
 const autoprefixer = require('autoprefixer')
 const jeet = require('jeet')
 const yeticss = require('yeticss')
-const Visualizer = require('webpack-visualizer-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
 	plugins: [
-		new Visualizer()
+		new webpack.optimize.DedupePlugin(),
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production')
+			}
+		}),
+		new ExtractTextPlugin('style.css')
 	],
-	devtool: 'eval-source-map',
+	devtool: 'cheap-module-source-map',
 	entry: './src/main.jsx',
 	output: {
 		path: './build',
@@ -27,7 +34,7 @@ module.exports = {
 		},
 		{
 			test: /\.css$/,
-			loaders: ['style', 'css', 'stylus', 'postcss']
+			loaders: ExtractTextPlugin.extract('style-loader', 'css-loader!stylus-loader!postcss-loader')
 		}]
 	},
 	stylus: {
