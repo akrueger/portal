f2bbd63567e4516e06044acd7cd797ca996d1f15
Andrew Krueger <krueger.andrew@gmail.com>
2016-08-07 10:47:03 -0500

    Add HTTPS to Koa server; remove redundancy in NPM build and start scripts

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const koa = require('koa')
+const http = require('http')
+const https = require('https')
+const fs = require('fs')
+const enforceHttps = require('koa-sslify')
 const serve = require('koa-static')
 const cacheControl = require('koa-cache-control')
 const compress = require('koa-compress')
 
 const app = koa()
+
+// Force HTTPS on all page
+app.use(enforceHttps())
 
 app.use(cacheControl({
 	public: true,
@@ -16,4 +23,11 @@ app.use(compress())
 
 app.use(serve('./build'))
 
-app.listen(3000)
+// SSL options
+const options = {
+	key: fs.readFileSync('server.key'),
+	cert: fs.readFileSync('server.crt')
+}
+
+http.createServer(app.callback()).listen(80)
+https.createServer(options, app.callback()).listen(443)
